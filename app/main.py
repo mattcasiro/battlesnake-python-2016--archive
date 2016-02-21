a332c7c68c3673afc978cabb067df8cc39786847
Zullybur <mattcasiro@gmail.com>
2016-02-20 16:31:38 -0800

    print statements don't fix bugs

--- app/main.py
+++ app/main.py
@@ -95,12 +95,13 @@ def move():
 
     # for x in occupied:
     #     print x
-    if "f729b53e-3477-447d-b07e-c79d7e326c82" in data['snakes']['id']:
-        print "Food target: {}" .format(bestFood)
-        print "Head: {}" .format(head)
-        print "posMoves: {}" .format(posMoves)
-        print "x diff: {}" .format(want[0])
-        print "y diff: {}" .format(want[1])
+    for snake in data['snakes']:
+        if snake['id'] == "f729b53e-3477-447d-b07e-c79d7e326c82":
+            print "Food target: {}" .format(bestFood)
+            print "Head: {}" .format(head)
+            print "posMoves: {}" .format(posMoves)
+            print "x diff: {}" .format(want[0])
+            print "y diff: {}" .format(want[1])
 
     for move in posMoves:
         if posMoves[move]:
